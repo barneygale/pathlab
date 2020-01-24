637b855f2f4b6ab9495e62769f8a377fd2f453bb
barneygale <barney.gale@gmail.com>
2020-01-24 23:58:55 +0000

    Flesh out `examples/zippath.py`

--- examples/zippath.py
+++ examples/zippath.py
@@ -1,7 +1,6 @@
 import datetime
 import io
 import posixpath
-import stat
 import pathlab
 import zipfile
 
@@ -20,72 +19,75 @@ class ZipAccessor(pathlab.Accessor):
             self.zipfile = file
         else:
             self.zipfile = zipfile.ZipFile(file, *args, **kwargs)
+
+        # Enumerate members
         self.infodict = {}
         for info in self.zipfile.infolist():
             filename = posixpath.normpath('/' + info.filename)
             self.infodict[filename] = info
 
     def __repr__(self):
-        return "ZipAccessor(%r)" % self.zipfile.filename
+        return 'ZipAccessor(%r)' % self.zipfile.filename
+
+    def getinfo(self, path):
+        path = str(path.absolute())
+        info = self.infodict.get(path)
+        if info is None and path != '/':
+            raise self.not_found(path)
+        return info
 
     # Re-implement ``open()``
     def open(self, path, mode='r', buffering=-1, encoding=None, errors=None,
              newline=None):
-        path1 = str(path.absolute())
-        info = self.infodict.get(path1)
-        if info is None:
-            raise self.not_found(path)
-        obj = self.zipfile.open(info, mode.replace('b', ''))
+        obj = self.zipfile.open(self.getinfo(path), mode.replace('b', ''))
         if 'b' not in mode:
             obj = io.TextIOWrapper(obj, encoding, errors, newline)
         return obj
 
-    # Re-implement ``os.listdir()``
-    def listdir(self, path):
+    # Re-implement ``os.scandir()``
+    def scandir(self, path):
+        # Check the directory exists
+        self.getinfo(path)
+
+        # Find children
         path1 = str(path.absolute())
-        found = path1 == '/'
-        children = []
-
         for path2, info in self.infodict.items():
-            # Look for the requested path
-            is_dir = info.filename[-1] == '/'
-            if path1 == path2:
-                if not is_dir:
-                    raise self.not_a_dir(path)
-                found = True
-
-            # Look for children of the requested path
             parent, name = posixpath.split(path2)
             if path1 == parent:
-                children.append(name)
+                yield self.ZipPath(path2)
 
-        if not found:
-            raise self.not_found(path)
-
-        return children
+    # Re-implement ``os.listdir()``
+    def listdir(self, path):
+        return [p.name for p in self.scandir(path)]
 
     # Re-implement ``os.stat()``
     def stat(self, path, follow_link=True):
-        path1 = str(path.absolute())
-
-        if path1 == '/':
-            result = pathlab.StatResult()
-            result.st_mode = stat.S_IFDIR
-            return result
-
-        info = self.infodict.get(path1)
-        if info is None:
-            raise self.not_found(path)
-
-        is_dir = info.filename[-1] == '/'
         result = pathlab.StatResult()
-        result.st_mode = stat.S_IFDIR if is_dir else stat.S_IFREG
-        result.st_ino = info.header_offset
         result.st_dev = id(self.zipfile)
-        result.st_size = info.file_size
-        result.st_mtime = int(datetime.datetime(*info.date_time).timestamp())
+        info = self.getinfo(path)
 
+        # Root directory
+        if info is None:
+            result.st_type = 'dir'
+
+        # Other path
+        else:
+            result.st_type = 'dir' if info.is_dir() else 'file'
+            result.st_ino = info.header_offset + 1
+            result.st_size = info.file_size
+            result.st_mtime = int(datetime.datetime(*info.date_time).timestamp())
         return result
 
     # Re-implement ``os.lstat()``
     lstat = stat
+
+    def upload(self, src, dst):
+        self.zipfile.write(src, str(dst.absolute()))
+
+    def download(self, src, dst):
+        with open(dst, 'wb') as fdst:
+            with self.zipfile.open(self.getinfo(src), 'rb') as fsrc:
+                fdst.write(fsrc.read())
+
+    def fspath(self, path):
+        return self.zipfile.extract(self.getinfo(path))
